f8891e4e7453afaad7fec3a089e4e54607e08892
Ahmed Wael <30728647+AhmedWael97@users.noreply.github.com>
2018-06-30 23:47:34 +0200

    Edit Names of Pages

---
+++ Partial/NavBar.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- bootstrap CSS LIB -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- bootstrap JQ Link -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<!-- GOOGLE FONT -->
+  	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+  	<!-- My Awsome CSS -->
+        
+      <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+
+  	<link rel="stylesheet" type="text/css" href="css/creativeStyle.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+      <script src="js/slick.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default container">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <img src="imgs/logo.png" class="BrandLogo">
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">DAY TRIPS</a></li>
+        <li><a href="#">TRANSFERS</a></li>
+        <li><a href="#">ACCOMDATION</a></li>
+        <li><a href="#">TOURGUIDES</a></li>
+        <li><a href="#">TICKETS</a></li>
+        <li><a href="#">FLIGHT</a></li>
+        <li><a href="#">CHECKOUT</a></li>
+       
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+</body>
+</html>
---
+++ Partial/footer.php
@@ -0,0 +1,105 @@
+
+		
+<section class="Section9">
+	<div class="row">
+		<div class="col-lg-3">
+			<p><b>GET In Touch</b></p>
+			<br>
+			<p>qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd.</p>
+			<p><span class="glyphicon glyphicon-home"></span> qwdqwd qwdqwd qwdqwd</p>
+			<p><span class="glyphicon glyphicon-earphone"></span> asd-qwdqwd qwdqwd POL</p>
+			<br>><br>
+			<img src="imgs/logo.png">
+		</div>
+		<div class="col-lg-3">
+			<p><b>QuickLinks</b></p>
+			<br>
+			<p>Awards</p>
+			<p>Link1</p>
+			<p>Link1</p>
+			<p>Link1</p>
+			<p>Link1</p>
+			<p>Link1</p>
+			<div class="Links"> 
+				<div class="row">
+					<div class="col-lg-4">
+						<p class="Link">Nile Cruises</p>
+					</div>
+					<div class="col-lg-4">
+						<p class="Link">Nile Cruises</p>
+					</div>
+					<div class="col-lg-4">
+						<p class="Link">Nile Cruises</p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<p><b>MILING LIST</b></p>
+			<br>
+			<p>qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd qwd</p>
+			<form>
+				 <div class="input-group">
+				      <input type="text" class="form-control" placeholder="Search for...">
+				      <span class="input-group-btn">
+				        <button class="btn btn-default" type="submit">
+				        	<span class="glyphicon glyphicon-send text">
+				        	</span>
+				        </button>
+				      </span>
+			    </div>
+			</form>
+			<div class="LinksPart2"> 
+				<div class="row">
+					<div class="col-lg-4">
+						<p class="Link">Nile Cruises</p>
+					</div>
+					<div class="col-lg-4">
+						<p class="Link">Nile Cruises</p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<p><b>FLICKER STEAM</b></p>
+			<br>
+			<img src="imgs/footerpic.jpg" width="100%">
+			<img src="imgs/skype.png" width="10%" class="SocialImage">
+			<img src="imgs/viper.png" width="10%" class="SocialImage">
+			<img src="imgs/w.png" width="10%" class="SocialImage">
+			<img src="imgs/youtupe.png" width="10%" class="SocialImage">
+			<img src="imgs/line.png" width="10%" class="SocialImage">
+			<img src="imgs/googel-plus.png" width="10%" class="SocialImage">
+			<img src="imgs/facebook.png" width="10%" class="SocialImage">
+		</div>
+	</div>
+	<br>
+	<br>
+	
+	<div>
+		<p><b>
+			© 2018 Go Travel Egypt | Copyright by Smart Geeks
+		</b></p>
+	</div>
+</section>
+
+
+
+ <script src="js/mixitup.min.js"></script>
+
+        <script>
+            var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
+
+            var mixer = mixitup(containerEl, {
+                selectors: {
+                    target: '[data-ref~="mixitup-target"]'
+                }
+            });
+        </script>
+		<script src="js/script.js"></script>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+</section>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="js/Slider.js"></script>
